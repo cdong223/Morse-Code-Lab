b022ec7235ebddbbfedd0b8bfa3f2261bd66590e
Claire Dong <cjd53@duke.edu>
2019-10-10 16:11:23 -0400

    got first E, 5, 9

--- lab2part3BROKENAHH.c
+++ lab2part3BROKENAHH.c
@@ -27,7 +27,7 @@
 #include <string.h>
 #define _XTAL_FREQ 4000000
 
-long int time, t0, t1, time_low, t0_low;
+long int time, t0, t1;
 int display_active = 0; //conditional for TMR1 interrupt
 int count = 0; //track spaces vs. pauses
 char letter[5] = "00000";
@@ -35,22 +35,24 @@ int index = 0;
 char d = 'd';
 char D = 'D';
 char e = 'e';
-char table[16][5] = {"DDDDD",
-                    "dDDDD",
-                    "ddDDD",
-                    "dddDD",
-                    "ddddD",
-                    "ddddd",
-                    "Ddddd",
-                    "DDddd",
-                    "DDDdd",
-                    "DDDDd",
-                    "dD000",
-                    "Dddd0",
-                    "DdDd0",
-                    "Ddd00",
-                    "d0000",
-                    "ddDd0"};
+char table[16][5] = {
+                    "DDDDD", //0
+                    "dDDDD", //1
+                    "ddDDD", //2
+                    "dddDD", //3
+                    "ddddD", //4
+                    "ddddd", //5 
+                    "Ddddd", //6
+                    "DDddd", //7
+                    "DDDdd", //8 
+                    "DDDDd", //9
+                    "dD000", //A
+                    "Dddd0", //B
+                    "DdDd0", //C
+                    "Ddd00", //D
+                    "d0000", //E
+                    "ddDd0"  //F
+};
 
 void display(int x){
     PORTAbits.RA0 = 1;
@@ -238,7 +240,6 @@ void display(int x){
     PORTAbits.RA0 = 0;
     memset(letter, '0', 5);
     index = 0;
-    count = 0;
     TMR1IF = 0;
     CCP1IF = 0;
     CCP2IF = 0;
@@ -264,7 +265,6 @@ void main(void){
     TRISB = 0x01; //RB0 = input
     TRISC = 0x06; //RC1(CCP2) and RC2(CCP1) set to input
     TMR1 = 0; //start timer at 0
-    TMR1IF = 0;
     INTCON = 0xC0; //enable peripheral/global interrupt
     T1CON = 0x01; //off, 1MHz, PS 1:1, always counts when on
     CCP1CON = 0x05; //CCP1 = rising edge
@@ -274,9 +274,6 @@ void main(void){
     PIR1 = 0x00; //set CCP1IF and TMR1IF to 0
     PIR2 = 0x00; //set CCP2IF to 0
 
-    time = 0;
-    time_low = 0;
-
     while(1){
     }
 }
@@ -285,6 +282,7 @@ void __interrupt() isr(void){
   if(CCP1IF == 1){ //triggered on rising edge
     t0 = CCPR1;
     display_active = 0;
+    count = 0;
     CCP1IF = 0;
   }
   if(TMR1IF == 1){ //timer overflows, add 65535 us to time
@@ -292,12 +290,12 @@ void __interrupt() isr(void){
     else{
       count = count + 1;
       if(count == 8) decode(letter);
-      TMR1 = 15535; //preload with 15535 so overflow happens at t=50ms
+      else TMR1 = 15535; //preload with 15535 so overflow happens at t=50ms
     }
     TMR1IF = 0;
   }
   if(CCP2IF == 1){ //triggered on falling edge
-    t1 = CCPR2;
+    t1 = CCPR2; 
     time = time + t1 - t0;
     if(time > 30000 && time < 200000) letter[index] = d; //dot
     else if(time > 200000 && time < 400000) letter[index] = D; //dash
